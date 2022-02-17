fa10435e6f9d362f5de4e551b9c3ecaeb75c7548
kha0213 <tatujjang@gmail.com>
2022-02-17 16:47:29 +0900

    Base entity 수정 (파일은 수정보다 삭제 후 다시 insert로 변경)

--- src/main/java/com/file/server/app/entity/BaseEntity.java
+++ src/main/java/com/file/server/app/entity/BaseEntity.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -23,8 +21,4 @@ public abstract class BaseEntity {
     @CreatedBy
     @Column(updatable=false)
     private String createBy;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
-    @LastModifiedBy
-    private String lastModifiedBy;
 }
