106ac250515c731428c454c0c1cde04b1a07df3b
kha0213 <tatujjang@gmail.com>
2022-02-17 17:13:21 +0900

    기본 createBy를 접속자 IP로 수정

--- src/main/java/com/file/server/app/config/BaseEntityAuditorAware.java
+++ src/main/java/com/file/server/app/config/BaseEntityAuditorAware.java
@@ -1,11 +1,11 @@
 package com.file.server.app.config;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Component
@@ -14,7 +14,28 @@ public class BaseEntityAuditorAware implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if(requestAttributes == null) return Optional.of("NONE");
-        return Optional.of(StringUtils.defaultString("", "NONE"));
+        if (requestAttributes == null) return Optional.of("SYSTEM");
+        return Optional.ofNullable(getClientIpAddr(requestAttributes.getRequest()));
+    }
+
+    private String getClientIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
